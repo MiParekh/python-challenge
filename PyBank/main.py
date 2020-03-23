0eac1ab15c385f06d4f22ecad79b6a4ffaecaa32
MiParekh <61482456+MiParekh@users.noreply.github.com>
2020-03-23 19:12:11 -0400

    Add files via upload

--- PyBank/main.py
+++ PyBank/main.py
@@ -7,14 +7,14 @@ import csv
 #Data File Name and Path to Open: C:\Users\mites\OneDrive\Documents\RU Data Science Bootcamp\python-challenge\PyBank\budget_data.csv
 #Read CSV File, identify comma delimiter, and print csvreader
 
-csvpath = os.path.join('OneDrive', 'Documents','RU Data Science Bootcamp', 'python-challenge', 'Pybank' ,'budget_data.csv')
+csvpath = "budget_data.csv"
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter= ',')
     print(csvreader)
 
-#Read Header Row and check print headers for confirmation (print commented out)
+#Read Header Row and check print headers for confirmation
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
+    
 
 # Create initial empty sets
     month = []
@@ -24,34 +24,33 @@ with open(csvpath) as csvfile:
     total_revenue_change = 0
     prev_month_rev = 0
 
-#Calculate Number of Months, and Sum Up total Profit or Loss in Budget Datafile (Print Checks Commented Out)
+#Calculate Number of Months, and Sum Up total Profit or Loss in Budget Datafile
     for row in csvreader:
         month.append(row[0])
         total_revenue = total_revenue + int(row[1])
         revenue_change = int(row[1]) - prev_month_rev
         prev_month_rev = int(row[1])
         monthly_revenue_chg.append(revenue_change)
-    #print(len(month))
-    #print(total_revenue)
+ 
 
 
-# Calculate average monthly change in budget datafile: Total Revenue Change / # Months and check via print (commented out)
+# Calculate average monthly change in budget datafile: Total Revenue Change / # Months 
     average_monthly_chg = round((sum(monthly_revenue_chg) - monthly_revenue_chg[0]) / (len(month) -1 ),2)
-    #print(average_monthly_chg)
     
-# Determine the greatest monthly profit increase amount, check via print (commented out), and identify month
+    
+# Determine the greatest monthly profit increase amount, and identify month
     largest_increase = max(monthly_revenue_chg)
-    #print(int(largest_increase))
+    
     lg_inc_mnth = monthly_revenue_chg.index(largest_increase)
     largest_increase_month = month[lg_inc_mnth]
-    #print(largest_increase_month)
+    
 
-# Determine the greatest monthly decrease change amount, check via print (commented out), and identify month
+# Determine the greatest monthly decrease change amount, and identify month
     largest_decrease = min(monthly_revenue_chg)
-    #print(int(largest_decrease))
+    
     lg_dec_mnth = monthly_revenue_chg.index(largest_decrease)
     largest_decrease_month = month[lg_dec_mnth]
-    #print(largest_decrease_month)
+   
   
 # Print Results on Terminal
 
